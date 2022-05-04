1e764007ac12e484ae69fe7da38b238598297938
rahimuj570 <rahimuj570@gmail.com>
2022-05-05 01:35:40 +0600

    My product is working

--- src/Components/Products/MyProducts.js
+++ src/Components/Products/MyProducts.js
@@ -1,7 +1,37 @@
 import React from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
 import { FaRegTrashAlt } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import auth from "../../firebase.int";
+import useGetData from "../../Hooks/useGetData";
 
 const MyProducts = () => {
+  const [user] = useAuthState(auth);
+  const navigate = useNavigate();
+  const [products, setProducts] = useGetData();
+
+  // ======== Delete Action =======
+  const deleteAction = (id) => {
+    const confirm = prompt(
+      `Are you sure to delete (${
+        products.find((product) => product._id === id).name
+      })? Then type "DELETE" to confirm your action.`
+    ).toLocaleUpperCase();
+    if (confirm === "DELETE") {
+      fetch(`http://localhost:5000/delete/${id}`, {
+        method: "DELETE",
+      }).then((res) =>
+        res.json().then((data) => {
+          const remainingPD = products.filter((product) => product._id !== id);
+          setProducts(remainingPD);
+        })
+      );
+    } else {
+      toast.error(`Type "DELETE" in Uppercase to Delete This Product`);
+      return;
+    }
+  };
   return (
     <>
       <div className=" pb-10 mt-16 ">
@@ -9,6 +39,15 @@ const MyProducts = () => {
           MY PRODUCTS
         </h1>
         <div className=" w-36 h-1 bg-indigo-400 mx-auto rounded-lg" />
+      </div>
+
+      <div className="mb-5 text-center">
+        <button
+          onClick={() => navigate("/add")}
+          className="p-1 font-semibold hover:bg-indigo-400 hover:text-white duration-300 hover:border-white sm:w-2/6 w-5/6 bg-white border-indigo-400 border-4 text-indigo-400"
+        >
+          Add New Products
+        </button>
       </div>
 
       <div className="pb-20 relative overflow-x-auto shadow-md sm:rounded-lg">
@@ -36,32 +75,35 @@ const MyProducts = () => {
             </tr>
           </thead>
           <tbody>
-            <tr className="border-b dark:bg-gray-800 dark:border-gray-700 odd:bg-white even:bg-gray-50 odd:dark:bg-gray-800 even:dark:bg-gray-700">
-              <td className="px-6 py-4">
-                <img
-                  className="w-10"
-                  src="chrome://branding/content/about-logo.png"
-                  alt=""
-                />
-              </td>
-              <th
-                scope="row"
-                className="px-6 py-4 font-medium text-gray-900 dark:text-white whitespace-nowrap"
-              >
-                Apple MacBook Pro 17"
-              </th>
-              <td className="px-6 py-4">HP</td>
-              <td className="px-6 py-4">Laptop</td>
-              <td className="px-6 py-4">$2999</td>
-              <td className="px-6 py-4 text-right">
-                <a>
-                  <FaRegTrashAlt
-                    title="Delete"
-                    className="text-red-600 cursor-pointer hover:text-red-500 text-2xl"
-                  />
-                </a>
-              </td>
-            </tr>
+            {products
+              .filter((pd) => pd.uid === user.uid)
+              ?.map(({ _id, name, price, supplier, image, quantity }) => (
+                <tr
+                  key={_id}
+                  className="border-b dark:bg-gray-800 dark:border-gray-700 odd:bg-white even:bg-gray-50 odd:dark:bg-gray-800 even:dark:bg-gray-700"
+                >
+                  <td className="px-6 py-4">
+                    <img className="w-10" src={image} alt="" />
+                  </td>
+                  <th
+                    scope="row"
+                    className="px-6 py-4 font-medium text-gray-900 dark:text-white whitespace-nowrap"
+                  >
+                    {name}
+                  </th>
+                  <td className="px-6 py-4">{supplier}</td>
+                  <td className="px-6 py-4">{quantity}</td>
+                  <td className="px-6 py-4">{price}</td>
+                  <td className="px-6 py-4 text-right">
+                    <div onClick={() => deleteAction(_id)}>
+                      <FaRegTrashAlt
+                        title="Delete"
+                        className="text-red-600 cursor-pointer hover:text-red-500 text-2xl"
+                      />
+                    </div>
+                  </td>
+                </tr>
+              ))}
           </tbody>
         </table>
       </div>
