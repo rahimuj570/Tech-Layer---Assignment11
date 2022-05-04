259b19191af908763ede7c3275bae651d34ef789
rahimuj570 <rahimuj570@gmail.com>
2022-05-04 18:23:32 +0600

    successfully dynamically display data in Home UI

--- src/Components/Products/Product.js
+++ src/Components/Products/Product.js
@@ -1,32 +1,35 @@
 import React from "react";
 
-const Product = () => {
+const Product = ({ product }) => {
+  const { _id, name, quentity, price, image, supplier, info } = product;
   return (
-    <div className="mb-4 rounded bg-white shadow ">
-      <div className="overflow-hidden w-4/6 h-48  shadow p-2 m-2 mx-auto ">
-        <img
-          className="hover:scale-110 duration-300 w-full h-full mx-auto"
-          src="chrome://branding/content/about-logo.png"
-          alt=""
-        />
-      </div>
-      <p className="mb-3 text-2xl text-center font-bold ">Firefox Browser</p>
-      <div className="font-signika bg-slate-50 p-2 text-base leading-7">
-        <p className="mb-1">
-          <span className="font-bold text-base">Supplier:</span> Firefox
-        </p>
-        <p className="mb-1" title="Per Product Price">
-          <span className="font-bold text-base">Price: </span> 10tk
-        </p>
-        <p className="mb-1">
-          <span className="font-bold text-base">Quantity: </span> 20
-        </p>
-        <p className="mb-1">
-          <span className="font-bold text-base">Short Info: </span>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Excepturi
-          nihil at totam voluptatibus mollitia minus ratione veritatis
-          praesentium fuga esse!
-        </p>
+    <div className="bg-slate-50 flex flex-col justify-between mb-4 rounded  shadow ">
+      <div className="">
+        <div className="bg-white px-1 pt-1 pb-1">
+          <div className=" overflow-hidden w-4/6 h-48  shadow p-2 m-2 mx-auto ">
+            <img
+              className="hover:scale-110 duration-300 w-full h-full mx-auto"
+              src={image}
+              alt={name}
+            />
+          </div>
+          <p className="mb-3 text-2xl text-center font-bold ">{name}</p>
+        </div>
+        <div className="font-signika p-2 text-base leading-7">
+          <p className="mb-1">
+            <span className="font-bold text-base">Supplier:</span> {supplier}
+          </p>
+          <p className="mb-1" title="Per Product Price">
+            <span className="font-bold text-base">Price: </span> {price}TK
+          </p>
+          <p className="mb-1">
+            <span className="font-bold text-base">Quantity: </span> {quentity}
+          </p>
+          <p className="mb-1">
+            <span className="font-bold text-base">Short Info: </span>
+            {info}
+          </p>
+        </div>{" "}
       </div>
       <button className="bg-indigo-400 text-white  px-2 py-1 hover:bg-indigo-500 duration-300 font-bold w-full">
         Update
